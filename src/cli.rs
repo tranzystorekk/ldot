36cd33dbdf03ee15ea486d24a405c7bdc8bd6839
Marcin Puc <tranzystorek.io@protonmail.com>
2022-09-28 23:29:59 +0200

    chore: bump clap version to 4.0

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::{IntoApp, Parser};
+use clap::{CommandFactory, Parser};
 use clap_complete::Shell;
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
@@ -11,37 +11,37 @@ pub fn generate_completion(shell: Shell) {
 
 /// List hidden files and directories AKA "dotfiles"
 #[derive(Parser)]
-#[clap(version)]
+#[command(version)]
 pub struct Cli {
     /// Use a long listing format
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub list: bool,
 
     /// Sort by file size, largest first
-    #[clap(short = 'S', long, overrides_with = "sort-by-time")]
+    #[arg(short = 'S', long, overrides_with = "sort-by-time")]
     pub sort_by_size: bool,
 
     /// Sort by time, newest first
-    #[clap(short = 't', long)]
+    #[arg(short = 't', long)]
     pub sort_by_time: bool,
 
     /// Reverse order when sorting
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub reverse: bool,
 
     /// With -l, print human-readable file sizes, e.g. 10M
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub human_readable: bool,
 
     /// Specify when to colorize the output
-    #[clap(long, possible_values = ["always", "auto", "never"], default_value = "auto")]
+    #[arg(long, value_parser = ["always", "auto", "never"], default_value = "auto")]
     pub color: String,
 
     /// Generate a shell completion script and exit
-    #[clap(long, value_name = "SHELL", arg_enum)]
+    #[arg(long, value_name = "SHELL", value_enum)]
     pub completion: Option<Shell>,
 
     /// List contents inside the specified directory
-    #[clap(value_name = "ROOT_DIR")]
+    #[arg(value_name = "ROOT_DIR")]
     pub from_dir: Option<PathBuf>,
 }
